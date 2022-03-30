92639207a009d8c4c3a57fdd52493a7d8c2dd1da
PabloPajares <100383549@alumnos.uc3m.es>
2022-03-30 16:31:10 +0200

    js

--- juego.js
+++ juego.js
@@ -1,2 +1,33 @@
-console.log("Proyecto de apps multimedia");
-//alert("La página funciona");
+window.onload = function(){
+    var tablero;
+    document.getElementById("boton").onclick = function inicio(){
+        tablero = document.getElementById("juego");
+        if(tablero && tablero.getContext){
+            var ctx = tablero.getContext("2d");
+
+            function tab(){
+                ctx.fillStyle = 'rgba(128, 175, 0, 0.9)';   //cambiar el color.
+                ctx.fillRect(0, 0, 800, 500);
+            
+                /*                  PARA PONER BORDES
+                ctx.beginPath();
+                ctx.moveTo(0,0);
+                ctx.lineTo(400,0);
+                ctx.lineTo(400,300);
+                ctx.lineTo(0,300);
+                ctx.lineWidth = 25;
+                ctx.strokeStyle = 'darkgreen';
+                ctx.stroke();
+                */
+            }
+
+            tab();
+
+            
+
+        }
+    }
+}
+
+
+
