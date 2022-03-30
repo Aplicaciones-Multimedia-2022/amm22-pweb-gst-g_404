28b5cdd1fe7cefc29666e09e27ac9aca5d818086
evaal02 <101268565+evaal02@users.noreply.github.com>
2022-03-30 16:50:45 +0200

    Update juego.js

--- juego.js
+++ juego.js
@@ -8,7 +8,7 @@ window.onload = function(){
             function tab(){
                 ctx.fillStyle = 'rgba(128, 175, 0, 0.9)';   //cambiar el color.
                 ctx.fillRect(0, 0, 800, 500);
-            
+
                 /*                  PARA PONER BORDES
                 ctx.beginPath();
                 ctx.moveTo(0,0);
@@ -22,12 +22,21 @@ window.onload = function(){
             }
 
             tab();
+            var homer = new Object();
 
-            
+            homer.pos_x = 50;
+            homer.pos_y = 250;
+
+            function pintar_homer() {
+              ctx.beginPath();
+              ctx.strokeStyle = 'white';
+              ctx.arc(homer.pos_x, homer.pos_y, 25, 0, 2 * Math.PI);
+              ctx.fillStyle = 'white';
+              ctx.fill();
+            }
+            pintar_homer();
+
 
         }
     }
 }
-
-
-
