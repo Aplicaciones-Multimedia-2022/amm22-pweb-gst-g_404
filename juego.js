da31a3cf71ee2fda90cc1c42e3988bc967e3f7ce
PabloPajares <100383549@alumnos.uc3m.es>
2022-04-01 12:49:06 +0200

    js

--- juego.js
+++ juego.js
@@ -1,17 +1,51 @@
 window.onload = function(){
     var tablero;
     document.getElementById("boton").onclick = function inicio(){
-      var tablero = document.getElementById("juego");
 
-      if(tablero && tablero.getContext){
-        var ctx = tablero.getContext("2d");
-        var marginLeft= 10;
+        var tablero = document.getElementById("juego");
 
-      //  var homerctx = homer.getContext("2d")
+        if(tablero && tablero.getContext){
+            /*
+            var ctx = tablero.getContext("2d");
+            //var marginLeft= 10;
+            //var homerctx = homer.getContext("2d")
 
-        function tab(){
-          ctx.fillStyle = 'lightpink';   //cambiar el color.
-          ctx.fillRect(0, 0, 800, 500);
+            
+            var upPress = False; //Para la flecha de arriba
+            var downPress = False; //Para la flecha de abajo
+
+            //Eventos de escucha del teclado
+            document.addEventListener("keydown", keyDownHandler, false); //para saber si se pulsa
+            document.addEventListener("keyup", keyUpHandler, false); //para saber si se deja de pulsar
+
+            //Funciones de detección de las teclas
+            function keyDownHandler(e) {
+                if(e.keyCode == 38) {
+                    upPress = true;
+                }else if(e.keyCode == 40) {
+                    downPress = true;
+                }
+            }
+
+            function keyUpHandler(e) {
+                if(e.keyCode == 38) {
+                    upPress = false;
+                }
+                else if(e.keyCode == 40) {
+                  downPress = false;
+                }
+            }
+
+            /* //Objeto de homer
+            var homer = new Image(100,100);
+            homer.src = '../imagenes/Jugador-Homer.png';  
+            homer.pos_y = tablero.height/2;
+            homer.pos_x = tablero.width/4;
+            */
+            
+            function tab(){
+                ctx.fillStyle = 'lightpink';   //cambiar el color.
+                ctx.fillRect(0, 0, 800, 500);
 
                 /*                  PARA PONER BORDES
                 ctx.beginPath();
@@ -23,79 +57,52 @@ window.onload = function(){
                 ctx.strokeStyle = 'darkgreen';
                 ctx.stroke();
                 */
-        }
-
-
-
-        function pintar_homer(life, score) {
-          var homer = new Image();
-          homer.src = 'imagenes/Jugador-Homer.jpeg';
-          homer.onload = function() {
-            ctx.drawImage(homer, homer.width, homer.height);
-          };
-          homer.pos_x = (tablero.height/2)-(homer.height/2);
-          homer.pos_y = (tablero.width - homer.width)/2 - marginLeft;
-          homer.life = life;
-          homer.score = score;
-          homer.dead = False;
-
-        }
-
-/*
-            function pintar_donuts(){
-              var x = canva.width-700;
-              var y = canva.height-20;
-              ctx.beginPath();
-              var donuts=new Images();
-              donuts.src = 'imagenes/donuts.png';
-              donuts.arc = (x, y);
             }
-*/
-/* ---------------------- MOVER A HOMER -------------------------------- */
+            tab();
 
-         var upPress = False; //Para la flecha de arriba
-         var downPress = False; //Para la flecha de abajo
+            function pintar_homer() {
+                var img = document.getElementById("homer");
+                ctx.drawImage(img, 100, 100, 200, 140);
+                /*var homero = new Image();
+                homero.src = '../imagenes/Jugador-Homer.jpeg'
+                homero.onload = function() {
+                    ctx.drawImage(homero.src, 200, 250);
+                };*/
+            }
+            //pintar_homer();
 
-         document.addEventListener("keydown", keyDownHandler, false); //para saber si se pulsa
-         document.addEventListener("keyup", keyUpHandler, false); //para saber si se deja de pulsar
+            /*
+            function pintar_donuts(){
+                var x = canva.width-700;
+                var y = canva.height-20;
+                ctx.beginPath();
+                var donuts=new Images();
+                donuts.src = 'imagenes/donuts.png';
+                donuts.arc = (x, y);
+            }
+            */
 
-         function keyDownHandler(e) {
-           if(e.keyCode == 38) {
-             upPress = true;
-           }else if(e.keyCode == 40) {
-             downPress = true;
-           }
-         }
+            /* ---------------------- FUNCION PARA PINTAR -------------------------------- */
+            /*
+            function pintar (){
+                ctx.clearRect(0, 0, tablero.width, tablero.height);
+                tab();
+                jugador = new pintar_homer(3,0);
+                pintar_donuts();
+                x -= 2;
 
-         function keyUpHandler(e) {
-           if(e.keyCode == 38) {
-             upPress = false;
-           }
-           else if(e.keyCode == 40) {
-             downPress = false;
-           }
-         }
+                if(upPress && homer.pos_y < tablero.width-homer.width) { //para el movimiento de la cara de homer
+                    homer.pos_y += 7;
+                }
+                else if(downPress && homer.pos_y > 0) {
+                    homer.pos_y -= 7;
+                }
 
-/* ---------------------- FUNCION PARA PINTAR -------------------------------- */
-  /*       function pintar (){
-           ctx.clearRect(0, 0, tablero.width, tablero.height);
-           tab();
-           jugador = new pintar_homer(3,0);
-            //  pintar_donuts();
-            //  x -= 2;
-
-           if(upPress && homer.pos_y < tablero.width-homer.width) { //para el movimiento de la cara de homer
-             homer.pos_y += 7;
-           }
-           else if(downPress && homer.pos_y > 0) {
-             homer.pos_y -= 7;
-           }
-
-         }
-         setInterval(pintar, 20);
-*/
-      }else{
-        return alert('No se ha podido crear el objeto canvas');
-      }
+            }
+            setInterval(pintar, 20);
+            */
+        }else{
+            return alert('No se ha podido crear el objeto canvas');
+        }
     }
 }
