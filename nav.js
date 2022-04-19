b3f5fcffbcc695c42021342434e970cd8d136e4b
PabloPajares <100383549@alumnos.uc3m.es>
2022-04-19 13:06:10 +0200

    menú desplegable

--- nav.js
+++ nav.js
@@ -1,145 +1,42 @@
 window.onload = function(){
+  	
 
-  //Al prinncipio solo se muestra la página de inicio.
-  document.getElementById("inicio").classList.add("mostrar");
-  document.getElementById("descripcion").classList.add("ocultar");
-  document.getElementById("juego").classList.add("ocultar");
-  document.getElementById("autores").classList.add("ocultar");
-  document.getElementById("multimedia").classList.add("ocultar");
-  document.getElementById("desarrollo").classList.add("ocultar");
-  document.getElementById("referencias").classList.add("ocultar");
+	document.getElementById("boton_sub").onclick = function() {myFunction()};
+	//Función de toggle al pulsar en el botón de menú
+	function myFunction() {
+		document.getElementById("submn_2").classList.toggle("show");
+	}
 
-  //Pestaña de inicio
-  document.getElementById("p0").onclick = function(){
-    document.getElementById("inicio").classList.add("mostrar");
-    document.getElementById("inicio").classList.remove("ocultar");
+	//toggle al pinchar en cada una de las semanas
+	document.getElementById("s1").onclick = function(){
+		document.getElementById("submn_2").classList.toggle("show");
 
-    document.getElementById("descripcion").classList.add("ocultar");
-    document.getElementById("descripcion").classList.remove("mostrar");
-    document.getElementById("juego").classList.add("ocultar");
-    document.getElementById("juego").classList.remove("mostrar");
-    document.getElementById("autores").classList.add("ocultar");
-    document.getElementById("autores").classList.remove("mostrar");
-    document.getElementById("multimedia").classList.add("ocultar");
-    document.getElementById("multimedia").classList.remove("mostrar");
-    document.getElementById("desarrollo").classList.add("ocultar");
-    document.getElementById("desarrollo").classList.remove("mostrar");
-    document.getElementById("referencias").classList.add("ocultar");
-    document.getElementById("referencias").classList.remove("mostrar");
+	}
+	document.getElementById("s2").onclick = function(){
+		document.getElementById("submn_2").classList.toggle("show");
 
-  }
-  //Pestaña de descripcion del juego
-  document.getElementById("p1").onclick = function(){
-    document.getElementById("inicio").classList.add("ocultar");
-    document.getElementById("inicio").classList.remove("mostrar");
+	}
+	document.getElementById("s3").onclick = function(){
+		document.getElementById("submn_2").classList.toggle("show");
 
-    document.getElementById("descripcion").classList.add("mostrar");
-    document.getElementById("descripcion").classList.remove("ocultar");
+	}
+	document.getElementById("s4").onclick = function(){
+		document.getElementById("submn_2").classList.toggle("show");
 
-    document.getElementById("juego").classList.add("ocultar");
-    document.getElementById("juego").classList.remove("mostrar");
-    document.getElementById("autores").classList.add("ocultar");
-    document.getElementById("autores").classList.remove("mostrar");
-    document.getElementById("multimedia").classList.add("ocultar");
-    document.getElementById("multimedia").classList.remove("mostrar");
-    document.getElementById("desarrollo").classList.add("ocultar");
-    document.getElementById("desarrollo").classList.remove("mostrar");
-    document.getElementById("referencias").classList.add("ocultar");
-    document.getElementById("referencias").classList.remove("mostrar");
-  }
-  //Pestaña de juego
-  document.getElementById("p2").onclick = function(){
-    document.getElementById("inicio").classList.add("ocultar");
-    document.getElementById("inicio").classList.remove("mostrar");
-    document.getElementById("descripcion").classList.add("ocultar");
-    document.getElementById("descripcion").classList.remove("mostrar");
+	}
+	document.getElementById("s5").onclick = function(){
+		document.getElementById("submn_2").classList.toggle("show");
 
-    document.getElementById("juego").classList.add("mostrar");
-    document.getElementById("juego").classList.remove("ocultar");
+	}
+	document.getElementById("s6").onclick = function(){
+		document.getElementById("submn_2").classList.toggle("show");
 
-    document.getElementById("autores").classList.add("ocultar");
-    document.getElementById("autores").classList.remove("mostrar");
-    document.getElementById("multimedia").classList.add("ocultar");
-    document.getElementById("multimedia").classList.remove("mostrar");
-    document.getElementById("desarrollo").classList.add("ocultar");
-    document.getElementById("desarrollo").classList.remove("mostrar");
-    document.getElementById("referencias").classList.add("ocultar");
-    document.getElementById("referencias").classList.remove("mostrar");
-  }
-  //Pestaña de autores
-  document.getElementById("p3").onclick = function(){
-    document.getElementById("inicio").classList.add("ocultar");
-    document.getElementById("inicio").classList.remove("mostrar");
-    document.getElementById("descripcion").classList.add("ocultar");
-    document.getElementById("descripcion").classList.remove("mostrar");
-    document.getElementById("juego").classList.add("ocultar");
-    document.getElementById("juego").classList.remove("mostrar");
+	}
+	document.getElementById("s7").onclick = function(){
+		document.getElementById("submn_2").classList.toggle("show");
 
-    document.getElementById("autores").classList.add("mostrar");
-    document.getElementById("autores").classList.remove("ocultar");
+	}
 
-    document.getElementById("multimedia").classList.add("ocultar");
-    document.getElementById("multimedia").classList.remove("mostrar");
-    document.getElementById("desarrollo").classList.add("ocultar");
-    document.getElementById("desarrollo").classList.remove("mostrar");
-    document.getElementById("referencias").classList.add("ocultar");
-    document.getElementById("referencias").classList.remove("mostrar");
-  }
-  //Pestaña de multimedia
-  document.getElementById("p4").onclick = function(){
-    document.getElementById("inicio").classList.add("ocultar");
-    document.getElementById("inicio").classList.remove("mostrar");
-    document.getElementById("descripcion").classList.add("ocultar");
-    document.getElementById("descripcion").classList.remove("mostrar");
-    document.getElementById("juego").classList.add("ocultar");
-    document.getElementById("juego").classList.remove("mostrar");
-    document.getElementById("autores").classList.add("ocultar");
-    document.getElementById("autores").classList.remove("mostrar");
-
-    document.getElementById("multimedia").classList.add("mostrar");
-    document.getElementById("multimedia").classList.remove("ocultar");
-
-    document.getElementById("desarrollo").classList.add("ocultar");
-    document.getElementById("desarrollo").classList.remove("mostrar");
-    document.getElementById("referencias").classList.add("ocultar");
-    document.getElementById("referencias").classList.remove("mostrar");
-  }
-  //Pestaña de desarrollo
-  document.getElementById("p5").onclick = function(){
-    document.getElementById("inicio").classList.add("ocultar");
-    document.getElementById("inicio").classList.remove("mostrar");
-    document.getElementById("descripcion").classList.add("ocultar");
-    document.getElementById("descripcion").classList.remove("mostrar");
-    document.getElementById("juego").classList.add("ocultar");
-    document.getElementById("juego").classList.remove("mostrar");
-    document.getElementById("autores").classList.add("ocultar");
-    document.getElementById("autores").classList.remove("mostrar");
-    document.getElementById("multimedia").classList.add("ocultar");
-    document.getElementById("multimedia").classList.remove("mostrar");
-
-    document.getElementById("desarrollo").classList.add("mostrar");
-    document.getElementById("desarrollo").classList.remove("ocultar");
-
-    document.getElementById("referencias").classList.add("ocultar");
-    document.getElementById("referencias").classList.remove("mostrar");
-  }
-  //Pestaña de referencias
-  document.getElementById("p6").onclick = function(){
-    document.getElementById("inicio").classList.add("ocultar");
-    document.getElementById("inicio").classList.remove("mostrar");
-    document.getElementById("descripcion").classList.add("ocultar");
-    document.getElementById("descripcion").classList.remove("mostrar");
-    document.getElementById("juego").classList.add("ocultar");
-    document.getElementById("juego").classList.remove("mostrar");
-    document.getElementById("autores").classList.add("ocultar");
-    document.getElementById("autores").classList.remove("mostrar");
-    document.getElementById("multimedia").classList.add("ocultar");
-    document.getElementById("multimedia").classList.remove("mostrar");
-    document.getElementById("desarrollo").classList.add("ocultar");
-    document.getElementById("desarrollo").classList.remove("mostrar");
-
-    document.getElementById("referencias").classList.add("mostrar");
-    document.getElementById("referencias").classList.remove("ocultar");
-  }
-
+	
+		
 }
